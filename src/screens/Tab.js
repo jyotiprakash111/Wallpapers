24091d5a140b400473ba4c38e0d5720dc7b351a0
Jyotiprakash <jyotiprakash@clayfin.com>
2022-09-19 21:47:01 +0530

    vector-icons now showing - #BUG

--- src/screens/Tab.js
+++ src/screens/Tab.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import SettingScreen from '../hoc/Counter';
 import Home2 from '../screens/home';
@@ -11,28 +11,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
     return (
         <Tab.Navigator
-            screenOptions={({ route }) => ({
-                // tabBarIcon: ({ focused, color, size }) => {
-                //     let iconName;
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-                //     if (route.name === 'Home') {
-                //         iconName = focused
-                //             ? (<Image style={styles.icon} source={require("../assets/Images/icons/user.png")} />)
-                //             : (<Image style={styles.icon} source={require("../assets/Images/icons/settings.png")} />)
-                //     } else if (route.name === 'Settings') {
-                //         iconName = focused ? <Image style={styles.icon} source={require("../assets/Images/icons/settings.png")} /> : <Image style={styles.icon} source={require("../assets/Images/icons/user.png")} />;
-                //     }
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
 
-                //     // You can return any component that you like here!
-                //     return <Image source={require("../assets/Images/icons/home.svg")} />;
-                // },
-                tabBarActiveTintColor: 'tomato',
-                tabBarInactiveTintColor: 'gray',
-            })}
-        >
-            <Tab.Screen name="Home" component={Home2}/>
-            <Tab.Screen name="Settings" component={SettingScreen} />
-        </Tab.Navigator>
+            // You can return any component that you like here!
+            // return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home2} />
+        <Tab.Screen name="Settings" component={SettingScreen} />
+      </Tab.Navigator>
     );
 }
 
